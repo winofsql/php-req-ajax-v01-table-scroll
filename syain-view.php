0ff1adbb26a7b4a03f3946bb8f5aee42bffa009c
sworc <lightbox@nifty.com>
2022-04-12 15:29:22 +0900

    Add files via upload

--- syain-view.php
+++ syain-view.php
@@ -7,7 +7,6 @@
 <title>社員問合せ</title>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.js"></script>
 
 <style>
 html,body,form {
